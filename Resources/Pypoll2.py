668ed97f91c706fbe2ac04b9c7c3ca4a153a332a
mdsinger <114033254+mdsinger@users.noreply.github.com>
2022-12-04 17:48:38 -0700

    Add files via upload

---
+++ Resources/Pypoll2.py
@@ -0,0 +1,28 @@
+
+# The data we need to retrieve
+# Assign a variable to the file to load and the path.
+
+file_to_load = 'election_results.csv'
+
+# Open the election results and read the file.
+
+with open(file_to_load) as election_data:
+
+    # To do: Perform analysis.
+
+    print(election_data)
+
+# Close File
+election_data.close()
+
+
+# The total number of votes cast
+
+# A complete list of candidates who received votes
+
+# The percentage of votes each candidate won
+
+# The total number of votes each candidate won
+
+# The winner of the election based on popular vote.
+
